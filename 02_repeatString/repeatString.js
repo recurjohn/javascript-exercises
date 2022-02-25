288fc881ebf0bd8a281333773f54f036ac220e05
John D <dab.cena420@gmail.com>
2022-02-24 18:18:23 -0800

    Added code to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+//let newString = "";
 
+const repeatString = function(word, timesToRepeat) {
+    if (timesToRepeat < 0) { //negative condition
+        return "ERROR";
+    }
+    
+    let newString = "";
+
+    for (i=0; i<timesToRepeat; i++) {
+        newString += word;
+    }
+    
+    return newString; 
 };
 
 // Do not edit below this line
