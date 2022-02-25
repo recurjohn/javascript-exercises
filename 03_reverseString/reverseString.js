91426a102a47d5b7ff4e60d96d232df1e1320dd9
John D <dab.cena420@gmail.com>
2022-02-24 18:32:11 -0800

    Added code to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split("");
+    let arrReversed = arr.reverse();
+    let newString = arrReversed.join("");
 
+    return newString;
 };
 
 // Do not edit below this line
