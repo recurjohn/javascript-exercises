cd5612910bada42fd89ce559fd15ca35b79a3762
John D <dab.cena420@gmail.com>
2022-02-25 18:17:44 -0800

    Cleared all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  fahrenheit = ((fahrenheit - 32) * (5/9));
 
+  if (Number.isInteger(fahrenheit) == false) {
+    fahrenheit = Number(fahrenheit.toFixed(1));
+  }
+  return fahrenheit;
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  celsius = (celsius * (9/5) + 32);
 
+  if (Number.isInteger(celsius) == false) {
+    celsius = Number(celsius.toFixed(1));
+  }
+  return celsius;
 };
 
 // Do not edit below this line
