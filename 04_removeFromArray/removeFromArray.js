95c6d7896f1c2a9ef7606db0c69b40a449baedff
John D <dab.cena420@gmail.com>
2022-02-25 17:24:29 -0800

    Cleared all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toBeRemoved) {
+    let numToBeRemoved = [...toBeRemoved]; //define array of all the parameters
 
+    for (let i=0; i < numToBeRemoved.length; i++) {
+        let index = arr.indexOf(numToBeRemoved[i]);
+
+        if (index == -1) {
+            console.log("Not in the array.");
+        } else {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
