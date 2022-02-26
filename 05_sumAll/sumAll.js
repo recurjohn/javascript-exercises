add269f8904e305aa7b90329efacd925b57195a3
John D <dab.cena420@gmail.com>
2022-02-25 17:43:55 -0800

    Cleared all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (start > end) { 
+        let placeholder = start; //start
+        start = end; //end
+        end = placeholder; //start
+    }
 
+    return ((end-start)+1) * (start + end) / 2; //shoutout algorithms
 };
 
 // Do not edit below this line
